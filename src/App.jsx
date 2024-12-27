e3f037bf0e21914e5934a8d4f904940035e1302d
enes polat <enespolat0001@gmail.com>
2024-12-27 14:29:10 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <Sidebar />
+      <div className="pl-64">
+        <Header />
+        <main className="p-6">
+        </main>
+      </div>
+    </div>
+  )
+}
+
