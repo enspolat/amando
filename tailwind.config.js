72374967262822f357e54bcf83b81e11bfe85e83
enes polat <enespolat0001@gmail.com>
2024-12-30 18:40:34 +0300

    add tailwind config gradient and some svg

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,13 @@ export default {
     extend: {
       backgroundImage: {
         'custom-gradient': 'linear-gradient(to right, #455EFF 0%, #B33BF6 44%, #FF5F14 88%)',
+        'custom-gradient-2': 'linear-gradient(to right, #B39FFF 0%, #6A1ED2 100%)',
+        'custom-gradient-3': 'linear-gradient(to right, #FD1F43 0%, #4BE0FF 100%)',
+        'custom-gradient-4': 'linear-gradient(to right, #FD3820 0%, #FF844B 100%)',
+
+
       },
+
     },
   },
   plugins: [],
