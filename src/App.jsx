90f809b53f1141e4ff3c7d68d6f21e5ca1128b80
enes polat <enespolat0001@gmail.com>
2024-12-30 18:37:51 +0300

    add pages route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import Header from './components/Header'
-import Sidebar from './components/MainSidebar'
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
+import Sidebar from './components/MainSidebar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Wallet from './pages/Wallet';
+import Dex from './pages/Dex';
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-900">
-      <Sidebar />
-      <div className="pl-64">
-        <Header />
-        <main className="p-6 w-full">
-        </main>
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-900">
+        <Sidebar />
+        <div className="pl-64">
+          <Header />
+          <main className="p-6 w-full">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/wallet" element={<Wallet />} />
+              <Route path="/dex" element={<Dex />} />
+            </Routes>
+          </main>
+        </div>
       </div>
-    </div>
-  )
+    </BrowserRouter>
+  );
 }
-
