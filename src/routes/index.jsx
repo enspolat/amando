dc1714027dca12df1e9ec1f4aac706a4a3c193f2
enes polat <enespolat0001@gmail.com>
2025-01-01 15:02:44 +0300

    add new routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,6 +3,10 @@ import MainLayout from '../layouts/MainLayout';
 import Home from "../pages/Home";
 import Wallet from '../pages/Wallet';
 import Dex from '../pages/Dex';
+import CreateWallet from '../pages/Wallet/CreateWallet';
+import BackupWallet from '../pages/Wallet/BackupWallet';
+import ConfirmSeedPhrase from '../pages/Wallet/ConfirmSeedPhrase';
+
 
 export const router = createBrowserRouter([
     {
@@ -14,8 +18,28 @@ export const router = createBrowserRouter([
                 element: <Home />
             },
             {
-                path: '/wallet',
-                element: <Wallet />
+                path: "wallet",
+                element: <Wallet />,
+                children: [
+                    {
+                        path: "create",
+                        element: <CreateWallet />,
+
+                        children: [
+                            {
+                                path: "backup",
+                                element: <BackupWallet />,
+                                children: [
+                                    {
+                                        path: "confirm",
+                                        element: <ConfirmSeedPhrase />
+                                    }
+                                ],
+                            }
+                        ],
+                    },
+
+                ]
             },
             {
                 path: '/dex',
